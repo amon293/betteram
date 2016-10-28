14e2d06d3c5a13d8f14b1658ee40d45a11a1e62d
Rafael <rafael.milewski@gmail.com>
2016-10-28 14:20:32 +0800

    First Attempt to improve the import code..

--- app/Console/Commands/ImportAirports.php
+++ app/Console/Commands/ImportAirports.php
@@ -3,45 +3,33 @@
 namespace App\Console\Commands;
 
 use App\Models\Airport;
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Console\Command;
+use Symfony\Component\DomCrawler\Crawler;
 
 class ImportAirports extends Command
 {
     protected $signature = 'import:wac';
     protected $description = 'Import the airports from https://www.world-airport-codes.com/';
+    protected $numAirports = 0;
+    public $promises = [];
 
     // Links from site to list airports by country
-    protected $links = [
-        'https://www.world-airport-codes.com/alphabetical/country-name/a.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/b.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/c.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/d.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/e.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/f.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/g.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/h.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/i.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/j.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/k.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/l.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/m.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/n.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/o.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/p.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/q.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/r.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/s.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/t.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/u.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/v.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/x.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/w.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/y.html',
-        'https://www.world-airport-codes.com/alphabetical/country-name/z.html'
-    ];
-
-    protected $numAirports = 0;
     protected $numImportErrors = 0;
+    private $base = 'https://www.world-airport-codes.com';
+    /**
+     * Remapping keys on database key name
+     *
+     * @var array
+     */
+    private $keys = [
+        'iata_code' => 'iata',
+        'icao_code' => 'icao',
+        'faa_code' => 'faa',
+        'time_zone' => 'timezone'
+    ];
 
     public function __construct()
     {
@@ -50,105 +38,128 @@ class ImportAirports extends Command
 
     public function handle()
     {
-        $this->line("Start to import airports from https://www.world-airport-codes.com/");
+        $this->line("Start to import airports from {$this->base}");
         $this->line("Start time: " . date("Y-m-d H:i:s"));
-        $this->search();
-        $this->info($this->numAirports . " found");
-        $this->info(($this->numAirports - $this->numImportErrors) . " airports imported");
 
-        if ($this->numImportErrors>0)
-            $this->error("See the error log for more info");
+        $this->search();
+
+        $this->info("{$this->numAirports} airports imported");
+
+//        if ($this->numImportErrors > 0)
+//            $this->error("See the error log for more info");
 
         $this->line("End time: " . date("Y-m-d H:i:s"));
     }
 
     protected function search()
     {
-        foreach ($this->links as $link) {
+        $client = new Client();
+        $pool = [];
 
-            try {
-                $dom = new \DOMDocument();
-                $dom->loadHTMLFile($link);
-                $dom->preserveWhiteSpace = false;
+        foreach (range('a', 'z') as $letter) {
 
-                $xPath = new \DOMXPath($dom);
-                $nodes = $xPath->query("//table[@class='responsive']//tr[@class='table-link']//th//a");
-            } catch (\Exception $e) {
-                $this->error($e->getMessage());
-                $this->numImportErrors++;
-                continue;
-            }
+            $promise = $client->getAsync(
+                "https://www.world-airport-codes.com/alphabetical/country-name/{$letter}.html"
+            );
 
-            foreach ($nodes as $node) {
-                try {
-                    $page = "https://www.world-airport-codes.com" . $node->getAttribute('href');
-                    $airport = $this->makeAirport($page);
+            $promise->then(function ($response) {
 
-                    $airport->save();
+                $crawler = new Crawler($response->getBody()->getContents());
 
-                    $this->numAirports++;
-                } catch (\Exception $e) {
-                    $this->error($e->getMessage());
-                    $this->numImportErrors++;
-                    continue;
-                }
-            }
+                $nodes = $crawler->filter(
+                    "table[class='responsive'] tr[class='table-link'] th a"
+                );
+
+                $client = new Client(['base_uri' => $this->base]);
+
+                $nodes->each(function (Crawler $node) use ($client) {
+
+                    $promise = $client->getAsync($node->attr('href'));
+
+                    $promise->then(function ($response) {
+                        $this->makeAirport($response);
+                    });
+
+                    array_push($this->promises, $promise);
+
+                });
+
+                $results = Promise\settle($this->promises)->wait();
+
+            });
+
+            array_push($pool, $promise);
+
         }
+
+        $results = Promise\settle($pool)->wait();
+
     }
 
-    protected function makeAirport($page) : Airport
+    protected function makeAirport(Response $response) : Airport
     {
 
-        $dom = new DOMDocument();
-        $dom->loadHTMLFile($page);
-        $dom->preserveWhiteSpace = false;
+        $crawler = new Crawler($response->getBody()->getContents());
 
-        $xPath = new DOMXPath($dom);
-
-        $runways = 0;
-        $elements = $xPath->query("//div[@class='small-12 medium-6 columns']");
-        foreach ($elements as $element) {
-            if (strstr($element->nodeValue, 'Runway')) {
-                $runways++;
-            }
-        }
-
-        $airPortName = trim($xPath->query("//h1[@class='airport-title']")[0]->nodeValue);
-
-        $cityCountry = explode(',', trim($xPath->query("//p[@class='subheader']")[0]->nodeValue));
-        $airPortCity = trim($cityCountry[0]) ?? "";
-        $airPortCountry = trim($cityCountry[1]) ?? "";
-
-        $airport = array(
-            'name' => $airPortName,
-            'city' => $airPortCity,
-            'country' => $airPortCountry,
-            'iata' => $xPath->query("//span[@data-key='IATA Code']")[0]->getAttribute('data-value'),
-            'icaoCode' => $xPath->query("//span[@data-key='ICAO Code']")[0]->getAttribute('data-value'),
-            'faaCode' => $xPath->query("//span[@data-key='FAA Code']")[0]->getAttribute('data-value'),
-            'phone' => $xPath->query("//span[@data-key='Phone']")[0]->getAttribute('data-value'),
-            'fax' => $xPath->query("//span[@data-key='Fax']")[0]->getAttribute('data-value'),
-            'latitude' => $xPath->query("//span[@data-key='Latitude']")[0]->getAttribute('data-value'),
-            'longitude' => $xPath->query("//span[@data-key='Longitude']")[0]->getAttribute('data-value'),
-            'timezone' => $xPath->query("//span[@data-key='Time Zone']")[0]->getAttribute('data-value'),
-            'email' => $xPath->query("//span[@data-key='Email']")[0]->getAttribute('data-value'),
-            'twitter' => $xPath->query("//span[@data-key='Twitter']")[0]->getAttribute('data-value'),
-            'facebook' => $xPath->query("//span[@data-key='Facebook']")[0]->getAttribute('data-value'),
-            'runways' => $runways
+        $nodes = $crawler->filter(
+            "div[class='small-12 medium-6 columns']"
         );
 
-        return new Airport($this->fix($airport));
+        $data = array_collapse($nodes->filter('span[data-key]')->each(function (Crawler $node) {
+            return [
+                $this->normalizeCase($node->attr('data-key')) => $node->text()
+            ];
+        }));
+
+        $address = explode(',', $this->clean($crawler->filter("p[class='subheader']")->text()));
+
+        $extra = [
+            'name' => $this->clean($crawler->filter("h1[class='airport-title']")->text()),
+            'city' => $this->clean($address[0]),
+            'country' => $this->clean($address[1])
+        ];
+
+        $this->numAirports++;
+
+        return (new Airport())->create(
+            $this->remap(array_merge($extra, $data))
+        );
+
     }
 
-    protected function fix($airport) {
-        // Sanatize the name of airport
-        $linksArray = explode(" ", html_entity_decode($airport['name']));
-        $linksArray = array_filter($linksArray);
-        $airport['name'] = implode(" ", $linksArray);
-
-        $newTimeZone = explode(' ', $airport['timezone']);
-        $airport['timezone'] = trim($newTimeZone[0]);
-
-        return $airport;
+    /**
+     * Transform UPPERCASE String -> uppercase_string
+     *
+     * @param string $phrase
+     * @return string
+     */
+    private function normalizeCase(string $phrase) : string
+    {
+        return snake_case(camel_case(strtolower($phrase)));
     }
+
+    /**
+     * Clean excess of white space in string
+     *
+     * @param string $string
+     * @return string
+     */
+    private function clean(string $string) : string
+    {
+        return str_replace('  ', ' ', preg_replace('/\s+/', ' ', trim($string)));
+    }
+
+    /**
+     * Remap Keys with desired keys uglyKeyName => prettier_key
+     *
+     * @param array $data
+     * @return array
+     */
+    private function remap(array $data) : array
+    {
+        return collect($data)->keyBy(function ($value, $key) {
+            return $this->keys[$key] ?? $key;
+        })->toArray();
+    }
+
 }
